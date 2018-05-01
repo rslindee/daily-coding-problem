49475228deeef6c5759543d206f882ccf97f86e1
Richard Slindee <katzinator@gmail.com>
2018-04-30 21:21:01 -0700

    Add 005 start, add 007 finished

---
+++ 007_message_encoding.py
@@ -0,0 +1,22 @@
+"""
+Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+
+For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+You can assume that the messages are decodable. For example, '001' is not allowed.
+"""
+# Create mapping
+mapping = [str(x) for x in range(1, 27)]
+
+def number_ways_decoded(message):
+    if not message:
+        return 1
+    count = 0
+    for i in mapping:
+        if message.startswith(i):
+            # Recursively count reset of message permutations
+            count += number_ways_decoded(message[len(i):])
+    return count
+
+
+assert number_ways_decoded('111') == 3
